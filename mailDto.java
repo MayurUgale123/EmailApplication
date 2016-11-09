3993c17e434d749719845bb646f4f751fd6ea39a
MayurUgale123 <mayurugale.94@gmail.com>
2016-11-09 16:04:07 +0530

    Create mailDto.java
    a pojo class

---
+++ mailDto.java
@@ -0,0 +1,56 @@
+package com.example;
+public class mailDto
+{
+private String fname;
+private String femail;
+private String toemail;
+private String cc;
+private String subject;
+private String bcc;
+private String body;
+
+public String getFname() {
+	return fname;
+}
+public void setFname(String fname) {
+	this.fname = fname;
+}
+public String getFemail() {
+	return femail;
+}
+public void setFemail(String femail) {
+	this.femail = femail;
+}
+public String getToemail() {
+	return toemail;
+}
+public void setToemail(String toemail) {
+	this.toemail = toemail;
+}
+public String getCc() {
+	return cc;
+}
+public void setCc(String cc) {
+	this.cc = cc;
+}
+public String getSubject() {
+	return subject;
+}
+public void setSubject(String subject) {
+	this.subject = subject;
+}
+public String getBcc() {
+	return bcc;
+}
+public void setBcc(String bcc) {
+	this.bcc = bcc;
+}
+public String getBody() {
+	return body;
+}
+public void setBody(String body) {
+	this.body = body;
+}
+
+
+}
