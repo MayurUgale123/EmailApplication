cb7ddc6d97cd15627aa2435ae344e65e2ce3c420
MayurUgale123 <mayurugale.94@gmail.com>
2016-11-09 16:38:54 +0530

    Create MayuremailApplication.java
    class having main function

---
+++ MayuremailApplication.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+//@ComponentScan
+public class MayuremailApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(MayuremailApplication.class, args);
+	}
+}
